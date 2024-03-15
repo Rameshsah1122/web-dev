eaf62593649cfd6b428ad4674f9729509d0fb297
Ramesh Sah <157626352+Rameshsah1122@users.noreply.github.com>
2024-03-15 23:53:48 +0530

    Add files via upload

---
+++ dark and light mode/script.js
@@ -0,0 +1,18 @@
+let mode = document.querySelector(".mode");
+let body = document.querySelector("body");
+let currMode = "light"; //change dark mode
+mode.addEventListener("click", () =>{ 
+    // mouseover
+    if(currMode === "light"){
+        currMode = "dark";
+        // document.querySelector("body").style.backgroundColor = "black";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }else{
+        currMode = "light";
+        // document.querySelector("body").style.backgroundColor = "white";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(currMode);
+});
